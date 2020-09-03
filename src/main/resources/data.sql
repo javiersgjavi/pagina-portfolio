5f4a1f1ce0966c2784e38ddab9083b7a18d1efb1
Javi <javsolgar@alum.us.es>
2020-09-03 17:35:43 +0200

    Proyecto 2 añadido

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,8 +9,25 @@ INSERT INTO user (id, email, first_name, last_name, password, username) VALUES (
 
 
 INSERT INTO proyecto (id, title, description, picture, url, url2, urld, urld2, conocimientos, date)
- VALUES (1,'Estudio de la demanda y precios de la electricidad (2017-2019)',
+ VALUES (1,
+'Estudio de la demanda y precios de la electricidad (2017-2019)',
 'Estudio en el que se aplican procesos de clustering, para agrupar datos de la demanda y precios de la electricidad, con el objetivo de, mediante el uso de una red neuronal LSTM, predecir los precios de la electricidad con 24 horas de antelación.',
-'/img/red_neuronal_p1.png','https://github.com/javiersgjavi/portfolio/blob/master/Estudio%20de%20electricidad%20(2020)/Estudio%20de%20la%20demanda%20y%20precios%20de%20la%20electricidad%20(2017-2019).ipynb',
-'https://colab.research.google.com/drive/1V4Rjfchb_nOktsxwjWlGw4sBPgc9DCR_?usp=sharing','Repositorio en GitHub',
-'Red neuronal en Google Colab','Redes neuronales, análisis de clustering','01/05/2020');
+'/img/red_neuronal_p1.png',
+'https://github.com/javiersgjavi/portfolio/blob/master/Estudio%20de%20electricidad%20(2020)/Estudio%20de%20la%20demanda%20y%20precios%20de%20la%20electricidad%20(2017-2019).ipynb',
+'https://colab.research.google.com/drive/1V4Rjfchb_nOktsxwjWlGw4sBPgc9DCR_?usp=sharing',
+'Repositorio en GitHub',
+'Red neuronal en Google Colab',
+'Redes neuronales,
+ análisis de clustering',
+ '01/05/2020');
+
+
+INSERT INTO proyecto (id, title, description, picture, url, urld, conocimientos, date)
+ VALUES (2,
+'Estudio sobre Q-Learning y algunas de sus variaciones',
+'Trabajo para la universidad, calificado con un 10, en el que se aplican diferentes versiones del algoritmo "Q-Learning" y "Sarsa" para encontrar el camino óptimo en un tablero.',
+'/img/q_learning_p2.png',
+'https://github.com/javiersgjavi/aprendizaje-refuerzo-IA',
+'Repositorio en GitHub',
+'Q-Learning, SARSA, visualización de grafos',
+ '16/06/2020');
